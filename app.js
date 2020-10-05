f418a8bc97b10b3f3eaf05be9fa2713edc325b10
Soumotanu Mazumdar <msoumotanu@gmail.com>
2020-10-06 01:00:12 +0530

    Finished

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import Redux from 'redux'
 // Insurance Company Analogy
 
 // Actions (form in insurance company analogy)
@@ -11,7 +12,7 @@ const createPolicy = (name, amount) => {
   }
 };
 
-const deletePolicy = () => {
+const deletePolicy = (name) => {
   return {
     type: 'DELETE_POLICY',
     payload: {
@@ -29,3 +30,52 @@ const createClaim = (name, amountToCollect) => {
     }
   }
 }
+
+// Reducers (departments in analogy)
+const claimsHistory = (oldClaims = [], action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return [...oldClaims, action.payload];
+  }
+  return oldClaims;
+};
+
+const accounting = (companyMoney=1000, action) => {
+  if (action.type === 'CREATE_CLAIM') {
+    return companyMoney - action.payload.amountToCollect;
+  } else if (action.type === 'CREATE_POLICY') {
+    return companyMoney + action.payload.amount;
+  }
+  return companyMoney;
+};
+
+const policies = (policiesList = [], action) => {
+  if (action.type === 'CREATE_POLICY') {
+    return [...policiesList, action.payload.name];
+  } else if (action.type === 'DELETE_POLICY') {
+    return policiesList.filter(name => name !== action.payload.name);
+  }
+  return policiesList;
+};
+
+const { createStore, combineReducers } = Redux;
+
+const ourDepartments = combineReducers({
+  accounting: accounting,
+  claimsHistory: claimsHistory,
+  policies: policies
+});
+
+const store = createStore(ourDepartments);
+
+const action = createPolicy('Adam', 200);
+store.dispatch(action)
+
+store.dispatch(createPolicy('Jim', 300));
+store.dispatch(createPolicy('John', 250));
+
+store.dispatch(createClaim('Jim', 600));
+store.dispatch(createClaim('John', 750));
+
+store.dispatch(deletePolicy('Adam'));
+
+console.log(store.getState());
